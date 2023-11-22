5a84c592d934ba9985e6d153ac6f9945d0560ce6
Alexandria1999 <130941963+Alexandria1999@users.noreply.github.com>
2023-11-21 21:01:28 -0300

    Add files via upload

---
+++ 02-03.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*comentário em várias linhas
+Daniel Gomes Alexandria
+*/
+int main()
+{
+    int valor;
+
+    valor = 50;
+
+    printf("\nO valor da minha variáel: %d\n\n", valor);
+
+    return 0;
+}
